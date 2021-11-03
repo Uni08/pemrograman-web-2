68f408da33fa3ed9c1d1c720cc13762a1af52409
sriwahyuningsih <sriwahyuningsih20@mhs.akba.ac.id>
2021-11-03 16:45:36 +0800

    Commit Pertama

--- tugas-02-01/variabel.php
+++ tugas-02-01/variabel.php
@@ -7,12 +7,4 @@ $usia = 25;
 echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya
 berusia ".$usia." tahun<p>";
 
-
-/**
-* Ini adalah variabel dengan nilai yang diubah
-*/
-$nama = Sri Wahyu Ningsih
-$usia = 25
-echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya
-berusia ".$usia." tahun<p>";
 ?>
